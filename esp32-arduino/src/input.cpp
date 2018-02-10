52464bfb01ad95028a814cb60f4828add52e1a60
KaiN <kain@piwnica.ws>
2018-02-10 20:53:50 +0100

    Added input edge detection, improved debounced, made it use micros()

--- esp32-arduino/src/input.cpp
+++ esp32-arduino/src/input.cpp
@@ -6,11 +6,13 @@ static uint8_t s_ubInputNext = 0;
 static tInput *s_pInputs[20];
 
 tInput::tInput(
-	uint8_t ubPin, tPullType ePull, bool isNegated, uint8_t ubDebounceMs
+	uint8_t ubPin, tPullType ePull, bool isNegated, uint32_t ulDebounceUs
 ):
 	m_ubPin(ubPin),
-	m_ubDebounceMs(ubDebounceMs),
-	m_isNegated(isNegated)
+	m_ulDebounceUs(ulDebounceUs),
+	m_isNegated(isNegated),
+	m_isActive(false),
+	m_wasActive(false)
 {
 	pinMode(m_ubPin, static_cast<uint8_t>(ePull));
 	s_pInputs[s_ubInputNext++] = this;
@@ -21,11 +23,39 @@ bool tInput::isActive(void)
 	return m_isActive ^ m_isNegated;
 }
 
+bool tInput::hasFallen(void)
+{
+	if(!m_isNegated) {
+		return !m_isActive && m_wasActive;
+	}
+	else {
+		return m_isActive && !m_wasActive;
+	}
+}
+
+bool tInput::hasRised(void)
+{
+	if(!m_isNegated) {
+		return m_isActive && !m_wasActive;
+	}
+	else {
+		return !m_isActive && m_wasActive;
+	}
+}
+
 void tInput::process(void)
 {
+	// Putting it here will ensure that edge detection occurs exactly one time
+	m_wasActive = m_isActive;
+
+	// Read current state and debounce
 	bool isNowActive = digitalRead(m_ubPin);
 	if(m_isDebouncing) {
-		if(millis() >= m_ubDebounceMs) {
+		if(isNowActive == m_isActive) {
+			// Still bouncing - reset timer
+			m_ulDebounceEnd = micros() + m_ulDebounceUs;
+		}
+		else if(micros() >= m_ulDebounceEnd) {
 			m_isDebouncing = false;
 			m_isActive = isNowActive;
 		}
@@ -33,7 +63,7 @@ void tInput::process(void)
 	else {
 		if(isNowActive != m_isActive) {
 			m_isDebouncing = true;
-			m_ulDebounceEnd = millis() + m_ubDebounceMs;
+			m_ulDebounceEnd = micros() + m_ulDebounceUs;
 		}
 	}
 }
--- esp32-arduino/src/input.h
+++ esp32-arduino/src/input.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <Arduino.h>
 
-#define INPUT_DEFAULT_DEBOUNCE 5
+#define INPUT_DEFAULT_DEBOUNCE_US 100
 
 enum class tPullType: uint8_t {
 	NONE = INPUT,
@@ -16,18 +16,23 @@ class tInput {
 	public:
 		tInput(
 			uint8_t ubPin, tPullType ePull = tPullType::NONE, bool isNegated = false,
-			uint8_t ubDebounceMs = INPUT_DEFAULT_DEBOUNCE
+			uint32_t ulDebounceUs = INPUT_DEFAULT_DEBOUNCE_US
 		);
 
 		bool isActive(void);
+
+		bool hasFallen(void);
+
+		bool hasRised(void);
 
 		void process(void);
 
 	private:
 		const uint8_t m_ubPin;
-		const uint8_t m_ubDebounceMs;
+		const uint32_t m_ulDebounceUs;
 		const bool m_isNegated;
 		bool m_isActive;
+		bool m_wasActive;
 		bool m_isDebouncing;
 		uint32_t m_ulDebounceEnd;
 };
